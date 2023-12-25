10c663708e167c4892fde4bd3b71828f896045b0
Mariusz <rexxarhell@gmail.com>
2023-12-25 12:58:08 +0100

    Implemented ConnectionPool with properties

---
+++ src/main/java/com/solvd/persistence/ConnectionPool.java
@@ -0,0 +1,85 @@
+package com.solvd.persistence;
+
+
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+
+public class ConnectionPool {
+
+    private static ConnectionPool connectionPool;
+    private static final String URL_KEY = "url";
+    private static final String USER_KEY = "user";
+    private static final String PASSWORD_KEY = "password";
+    private static final String DRIVER_KEY = "driver";
+    private static final String POOL_SIZE_KEY = "size";
+    private static BlockingQueue<Connection> pool;
+    private static List<Connection> sourceConnections;
+
+    private ConnectionPool() {
+
+    }
+
+    static {
+        loadDriver();
+        initConnectionPool();
+    }
+
+    private static void loadDriver() {
+        try {
+            Class.forName(PropertiesUtil.get(DRIVER_KEY));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Connection open() {
+        try {
+            return DriverManager.getConnection(PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USER_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void initConnectionPool() {
+        int size = Integer.parseInt(PropertiesUtil.get(POOL_SIZE_KEY));
+        pool = new ArrayBlockingQueue<>(size);
+        sourceConnections = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            Connection connection = open();
+            Connection proxyConnections = (Connection) Proxy.newProxyInstance(ConnectionPool.class.getClassLoader(),
+                    new Class[]{Connection.class},
+                    (proxy, method, args) -> method.getName().equals("close") ? pool.add((Connection) proxy)
+                            : method.invoke(connection, args));
+            pool.add(proxyConnections);
+            sourceConnections.add(connection);
+        }
+    }
+
+    public static Connection get() {
+        try {
+            return pool.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnectionPool() {
+        try {
+            for (Connection sourceConnection : sourceConnections) {
+                sourceConnection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
